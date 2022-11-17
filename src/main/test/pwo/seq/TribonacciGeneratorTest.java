be9bd074e2e3f7f8f4a7e48471e50c760d905d08
matashaEs <natali.babinska@ukr.net>
2022-11-17 22:44:48 +0100

    lab 12

---
+++ src/main/test/pwo/seq/TribonacciGeneratorTest.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package pwo.seq;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.math.BigDecimal;
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ *
+ * @author natal
+ */
+class TribonacciGeneratorTest {
+    Generator instance;
+
+    @BeforeEach
+    public void setup() {
+        instance = new LucasGenerator();
+    }
+
+    @Test
+    public void testReset() {
+        System.out.println("reset");
+        instance.reset();
+        BigDecimal expResult = new BigDecimal(0);
+        BigDecimal result = instance.nextTerm();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testNextTerm() {
+        instance.nextTerm();
+        BigDecimal expResult = new BigDecimal(0);
+        BigDecimal result = instance.nextTerm();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    void testGetTerm() {
+        BigDecimal expResult = new BigDecimal(76);
+        BigDecimal result = instance.getTerm(10);
+        assertEquals(expResult, result);
+    }
+}
